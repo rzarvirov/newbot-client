b05fe3700a10aa969965eb6dac14f904a4b5ab20
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-07 21:07:12 +0000

    title

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex flex-col min-h-screen text-slate-300">
         <header class="px-3 py-6 text-center">
-            <h1 class="text-3xl font-black text-white/80 drop-shadow-md">ChatGPT Web Client</h1>
+            <h1 class="text-3xl font-black text-white/80 drop-shadow-md">ChatGPT API: gpt-3.5-turbo</h1>
         </header>
         <main class="flex flex-grow px-3 py-2">
             <Chat/>
         </main>
         <footer class="px-3 py-6 text-center">
-            <span class="text-xs font-light text-slate-400">powered by <a href="https://github.com/waylaidwanderer/node-chatgpt-api" target="_blank">https://github.com/waylaidwanderer/node-chatgpt-api</a></span>
+            <span class="text-xs font-light text-slate-400">powered by ChatGPT API: gpt-3.5-turbo model</a></span>
         </footer>
     </div>
 </template>
