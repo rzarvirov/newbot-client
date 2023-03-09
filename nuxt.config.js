00785569571abf40d88bbc8a1f45c8e0c880f6e4
rzarvirov <127051122+rzarvirov@users.noreply.github.com>
2023-03-09 14:15:15 +0000

    footer

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindTypography from '@tailwindcss/typography';
+
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss',
-        'nuxt-icon',
-    ],
-    css: [
-        {
-            src: '~/node_modules/highlight.js/styles/base16/dracula.css',
-            lang: 'css',
-        },
-    ],
-    runtimeConfig: {
-        public: {
-            apiBaseUrl: process.env.API_BASE_URL,
-        },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+  ],
+
+  css: [
+    {
+      src: '~/node_modules/highlight.js/styles/base16/dracula.css',
+      lang: 'css',
     },
+  ],
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL,
+    },
+  },
 });
+
